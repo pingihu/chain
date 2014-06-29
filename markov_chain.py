27d38b4ee8619a2ab47a13240d8981c92e442c2b
IanWyszynski <Ian_Wyszynski@brown.edu>
2014-06-29 12:58:12 -0400

    switched to iterative method

--- markov_chain.py
+++ markov_chain.py
@@ -12,7 +12,7 @@ class MarkovChainWalker:
         self.ctr = 0
 
     def nextWord(self, curr_word):
-        num = random.randint(0,100)
+        num = random.randint(0, 100)
         if self.graph.nodes[curr_word] != []:
             total = 0
             for edge in self.graph.nodes[curr_word]:
@@ -25,13 +25,13 @@ class MarkovChainWalker:
     def walk(self, curr_node):
         sys.stdout.write(curr_node + " ")
         next = self.nextWord(curr_node)
-        if self.ctr < self.max_words:
-            self.ctr += 1
+        while self.ctr < self.max_words:
             if next != -1:
-                self.walk(next)
+                sys.stdout.write(next + " ")
+                next = self.nextWord(next)
             else:
-                source = self.graph.nodes.keys()[random.randint(0,len(self.graph.nodes) - 1)]
-                self.walk(source)
+                next = self.graph.nodes.keys()[random.randint(0, len(self.graph.nodes) - 1)]
+            self.ctr += 1
 
     def start(self):
         source = self.graph.nodes.keys()[random.randint(0,len(self.graph.nodes) - 1)]
@@ -39,10 +39,11 @@ class MarkovChainWalker:
 
 
 def main():
-    if (len(sys.argv) < 2):
-        print "usage : requires a text file"
-    if (len(sys.argv) > 2):
-        print "usage : only one text file at a time!"
+    num_words = 100
+    if len(sys.argv) == 2:
+        spec = str(raw_input("specify number of wbords? (y/n, default: 100)"))
+        if spec == "y":
+            num_words = int(raw_input("number of words: "))
     f = open(sys.argv[1], "r")
     text = f.read()
     for p in string.punctuation:
@@ -52,8 +53,8 @@ def main():
     for i in xrange(0, len(textAsList) - 1):
         markovGraph.addNode(textAsList[i], textAsList[i+1])
 
-    markovWalker = MarkovChainWalker(markovGraph, 100)
+    markovWalker = MarkovChainWalker(markovGraph, num_words)
     markovWalker.start()
     sys.stdout.write(".")
-    
+
 main()
